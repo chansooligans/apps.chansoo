2069ce6cbc46d236cbd435b4c65c641dd3f1d2f3
chansooligans <cs2737@nyu.edu>
2023-02-01 22:13:47 -0500

    merged file

--- centraljersey/merge.py
+++ centraljersey/merge.py
@@ -17,7 +17,7 @@ import geopandas as gpd
 class Merge:
 
     def __init__(self):
-        self.dfcensus = census.Census().nj_data.drop_duplicates(subset="tract_name")
+        self.census = census.Census().nj_data.drop_duplicates(subset="tract_name")
         self.dunkin = foursquare.Foursquare(company="dunkin").df_dunkins
         self.wawa = foursquare.Foursquare(company="wawa").df_wawas
         self.nfl = njdotcom.Njdotcom().nfl
@@ -27,7 +27,7 @@ class Merge:
         self.counties = self.counties.to_crs("EPSG:4269")
 
     @cached_property
-    def df(self):
+    def tracts(self):
         # Perform the spatial merge
         merged = gpd.sjoin(
             self.tracts, 
@@ -36,4 +36,46 @@ class Merge:
             op="intersects"
         )
         df = merged.drop_duplicates(subset="TRACTCE")
+        df = df.merge(self.nfl).merge(self.pork)
+        df = df.merge(self.census, how='left', left_on = "TRACTCE", right_on="tract")
+        # df.fillna(0).to_file("../data/merged_tracts.geojson", driver='GeoJSON')
+
+        return df
+
+    @cached_property
+    def counties(self):
+
+        merged = gpd.sjoin(self.counties, self.tracts, how="inner", op="intersects")
+        df = merged.drop_duplicates(subset="TRACTCE")
+        df = df[["COUNTY","COUNTYFP","geometry"]].drop_duplicates()
+        df = df.merge(self.nfl).merge(self.pork)
+        for col in self.census.columns:
+            if col not in ["tract_name","tract","county"]:
+                self.census[col] = self.census[col].astype(float)
+        census_county = (
+            self.census
+            .groupby("county")
+            .agg({
+                x:sum for x in census.columns 
+                if x not in ["tract_name","tract","county"]
+                })
+            .reset_index()
+        )
+        df = df.merge(
+            census_county, how='left', left_on = "COUNTYFP", right_on="county")
+
+        df = df.merge(
+            self.df_dunkins, 
+            how="left", 
+            left_on="COUNTYFP",
+            right_on="dunkin_county"
+        )
+
+        df = df.merge(
+            self.df_wawas, 
+            how="left", 
+            left_on="COUNTYFP",
+            right_on="wawa_county"
+        )
+
         return df
