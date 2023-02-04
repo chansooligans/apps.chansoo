6ddabcf84148df75078fd014e60141a1ff3bc780
Debian <bitnami@ip-172-26-14-65.ec2.internal>
2023-02-04 00:30:05 +0000

    only using leafllet now

--- jerseyproj/mapapp/views.py
+++ jerseyproj/mapapp/views.py
@@ -4,34 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'mapapp/index.html')
-
-def wawa(request):
-    return render(request, 'maps/wawa.html')
-
-def dunkin(request):
-    return render(request, 'maps/dunkin.html')
-
-def edu_college(request):
-    return render(request, 'maps/edu_college.html')
-
-def nfl_eagles(request):
-    return render(request, 'maps/nfl_eagles.html')
-
-def nfl_giants(request):
-    return render(request, 'maps/nfl_giants.html')
-
-def pork_pork_roll(request):
-    return render(request, 'maps/pork_pork_roll.html')
-
-def pork_taylor_ham(request):
-    return render(request, 'maps/pork_taylor_ham.html')
-
-def pob_native_jeresy(request):
-    return render(request, 'maps/pob_native_jeresy.html')
-
-def pob_foreigh_born(request):
-    return render(request, 'maps/pob_foreigh_born.html')
-
-def leaflet(request):
     return render(request, 'mapapp/leaflet.html')
