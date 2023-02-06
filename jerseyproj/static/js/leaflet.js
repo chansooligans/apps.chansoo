e2a2930ffe857e4777ebc6259b9e55dd7b24ac96
chansooligans <cs2737@nyu.edu>
2023-02-06 15:06:43 -0500

    add % sign to hover

--- jerseyproj/static/js/leaflet.js
+++ jerseyproj/static/js/leaflet.js
@@ -131,7 +131,7 @@ export const updateMap = function (column, geojson_url) {
 
         function onEachFeature(feature, layer) {
             layer.bindTooltip(
-                "Value: " + Math.round(feature.properties[column] * 1000) / 1000,
+                "Value: " + Math.round(feature.properties[column] * 1000) / 1000 + "%",
                 { permanent: false, direction: 'center', className: 'myCSSClass' }
             );
             layer.on({
