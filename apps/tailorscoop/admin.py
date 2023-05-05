e195dce4bfca1563a32a3ba691ddbead20f476b0
chansooligans <cs2737@nyu.edu>
2023-05-05 12:12:31 -0400

    testing

--- apps/tailorscoop/admin.py
+++ apps/tailorscoop/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import NewsletterSubscription, Today
+from .models import NewsletterSubscription
 
 admin.site.register(NewsletterSubscription)
-admin.site.register(Today)
