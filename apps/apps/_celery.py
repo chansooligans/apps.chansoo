0844c54ae185535cc1b16c07062f9130a6373b02
chansooligans <cs2737@nyu.edu>
2023-05-04 21:19:44 -0400

    remove tailorscoop module

--- apps/apps/_celery.py
+++ apps/apps/_celery.py
@@ -24,9 +24,5 @@ app.conf.beat_schedule = {
         'task': 'send_event_reminders',  
         # Schedule      
         'schedule': 60.0
-    },
-    # 'send_daily_newsletter': {
-    #     'task': 'send_daily_newsletter',
-    #     'schedule': crontab(hour=10, minute=0),
-    # },
+    }
 }
