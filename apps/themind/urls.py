caa3ade6ef024797543366658206068010e57ecb
chansooligans <cs2737@nyu.edu>
2023-09-19 23:14:59 -0400

    edit css

--- apps/themind/urls.py
+++ apps/themind/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='themind'),
+    path("room/<str:room_name>/", views.room, name="room"),
     path('reset', views.reset, name='reset'),
     path('delete-all-numbers/', views.delete_all_numbers, name='delete_all_numbers'),
+    path('set-room-name/', views.set_room_name, name='set_room_name'),
 ]
--- apps/themind/views.py
+++ apps/themind/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
 # views.py
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 from .models import RandomNumber
 import random
 
@@ -21,10 +24,32 @@ def home(request):
 
     return render(request, 'themind/home.html', context)
 
+def room(request, room_name):
+
+    generated_pairs = RandomNumber.objects.all()
+    all_numbers = set(range(1, 101))
+    remaining_numbers = all_numbers - {pair.value1 for pair in generated_pairs} - {pair.value2 for pair in generated_pairs}
+    
+    if len(remaining_numbers) < 2:
+        context = {"message": "Not enough numbers remaining to generate a unique pair!", "room_name": room_name}
+    else:
+        # Randomly select two unique numbers from the remaining numbers
+        number1, number2 = random.sample(remaining_numbers, 2)
+        RandomNumber.objects.create(value1=number1, value2=number2)
+        context = {"number1": number1, "number2": number2, "room_name":room_name}
+
+    return render(request, 'themind/room.html', context)
+
 def reset(request):
     return render(request, 'themind/reset.html')
 
 def delete_all_numbers(request):
     RandomNumber.objects.all().delete()
-    print(1)
     return redirect('reset') 
+
+def set_room_name(request):
+    if request.method == "POST":
+        room_name = request.POST.get("room_name")
+        request.session["room_name"] = room_name
+        return redirect(f'/themind/room/{room_name}')
+    return HttpResponse("Invalid request.", status=400)
